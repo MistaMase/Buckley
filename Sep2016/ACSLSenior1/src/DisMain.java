4aa4a4dbd0ac059ac7ab94fc32afbec8d402c032
MistaMase <jjjmase123@gmail.com>
2016-11-21 10:12:56 -0800

    Update DisMain.java

--- Sep2016/ACSLSenior1/src/DisMain.java
+++ Sep2016/ACSLSenior1/src/DisMain.java
@@ -7,7 +7,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class DisMain 
+public class he 
 {
 	public static void main(String []args)
 	{
@@ -84,7 +84,7 @@ public class DisMain
 
 			if(value.size() == 1)
 			{
-				System.out.println("Place 1\n");
+				//System.out.println("Place 1\n");
 				if(value.get(0) == "0")
 					return "Returns: " + "A" + suit.get(0);
 				else if(value.get(0) == "10")
@@ -113,7 +113,7 @@ public class DisMain
 				for(String i: value)
 					if(lowest > Integer.parseInt(i))
 						lowest = Integer.parseInt(i);
-				System.out.println("Place 2\n");
+				//System.out.println("Place 2\n");
 				return printProperly(suit, value, lowest);
 			}
 			else
@@ -122,7 +122,7 @@ public class DisMain
 				for(String i: value)
 					if(Integer.parseInt(i) > Integer.valueOf(key) && Integer.parseInt(i) < lowestHighest)
 						lowestHighest = Integer.parseInt(i);
-				System.out.println("Place 3\n");
+				//System.out.println("Place 3\n");
 				return printProperly(suit, value, lowestHighest);
 			}
 		}
@@ -136,7 +136,7 @@ public class DisMain
 					checkForSameLowest = Integer.parseInt(i);
 			if(checkForSameLowest == Integer.parseInt(key))
 			{
-				System.out.println("Place 4\n");
+				//System.out.println("Place 4\n");
 				if(suit.contains("C"))
 					return printProperlyValueGivenSuit(value, checkForSameLowest, "C");
 				else if(suit.contains("D"))
@@ -151,14 +151,14 @@ public class DisMain
 			for(String i: value)
 				if(lowest > Integer.parseInt(i))
 					lowest = Integer.parseInt(i);
-			System.out.println("Place 5\n");
+			//System.out.println("Place 5\n");
 			return printProperly(suit, value, lowest);
 		}
 	}
 
 	public static String printProperly(ArrayList<String> suit, ArrayList<String> value, int reason)
 	{
-		System.out.println(suit + "\n" + value + "\n" + reason);
+		//System.out.println(suit + "\n" + value + "\n" + reason);
 		if(value.get(value.indexOf(reason + "")) == "0")
 			return "Returns: " + "A" + suit.get(value.indexOf(reason + ""));
 		else if(value.get(value.indexOf(reason + "")) == "10")
@@ -175,7 +175,7 @@ public class DisMain
 
 	public static String printProperlyValueGivenSuit(ArrayList<String> value, int reason, String theSuit)
 	{
-		System.out.println(value + "\n" + reason + "\n" + theSuit);
+		//System.out.println(value + "\n" + reason + "\n" + theSuit);
 		if(value.get(value.indexOf(reason + "")) == "0")
 			return "Returns: " + "A" + theSuit;
 		else if(value.get(value.indexOf(reason + "")) == "10")
